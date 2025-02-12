6714c29e084513433a7ed3268c62c9418e4f8114
ZARROUK Anis <anis.zarrouk@witik.io>
2025-02-12 09:02:41 +0100

    update image of projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,22 @@
 export const projects = [
     {
       id: 1,
-      projectImg: require('../assets/coming-soon.jpg'),
-      name: 'Task Manager App',
+      projectImg: require('../assets/project-1.webp'),
+      name: 'AZ Task',
       desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m',
       category:'Web App'
     },
     {
       id: 2,
-      projectImg: require('../assets/coming-soon.jpg'),
-      name: 'Blog App',
+      projectImg: require('../assets/project-2.webp'),
+      name: 'AZ Blog',
       desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m',
       category:'Web App'
     },
     {
       id: 3,
-      projectImg: require('../assets/coming-soon.jpg'),
-      name: 'Social Network App',
+      projectImg: require('../assets/project-3.webp'),
+      name: 'AZ Link',
       desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m',
       category:'Web App'
     },
