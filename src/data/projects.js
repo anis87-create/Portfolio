6890175affb7ab2e1cb8b058368cc75d87b584e2
ZARROUK Anis <anis.zarrouk@witik.io>
2025-02-13 21:55:49 +0100

    add description of projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,35 +3,35 @@ export const projects = [
       id: 1,
       projectImg: require('../assets/project-1.webp'),
       name: 'AZ Task',
-      desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m',
+      desc:' Une application de gestion de tâches intuitive et performante, idéale pour organiser et suivre son travail. Elle permet de créer, assigner et prioriser des tâches efficacement.',
       category:'Web App'
     },
     {
       id: 2,
       projectImg: require('../assets/project-2.webp'),
       name: 'AZ Blog',
-      desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m',
+      desc:'Une plateforme de blogging moderne pour partager des articles et du contenu enrichissant. Avec une gestion intuitive des articles et une interface épurée, elle offre une expérience utilisateur fluide.',
       category:'Web App'
     },
     {
       id: 3,
       projectImg: require('../assets/project-3.webp'),
       name: 'AZ Link',
-      desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m',
+      desc:' Un réseau social moderne conçu pour faciliter la connexion et le partage entre utilisateurs. Il permet de publier du contenu, d’interagir avec la communauté et de créer des discussions engageantes.',
       category:'Web App'
     },
     {
       id: 4,
-      projectImg: require('../assets/coming-soon.jpg'),
-      name: 'Food Delivery App',
-      desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m',
+      projectImg: require('../assets/project-4.webp'),
+      name: 'AZ Delivery',
+      desc:'Une application optimisée pour la gestion des livraisons et suivis en temps réel. Elle offre un suivi précis des colis, une gestion simplifiée des commandes et des notifications instantanées. Une solution parfaite pour les entreprises de logistique et e-commerce.',
       category:'Mobile App'
     },
     {
       id: 4,
-      projectImg: require('../assets/coming-soon.jpg'),
-      name: 'Streaming App',
-      desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m',
+      projectImg: require('../assets/project-5.webp'),
+      name: 'AZ Stream',
+      desc:'Une plateforme de streaming immersive avec une interface fluide et une navigation optimisée. Elle permet d’accéder à une large bibliothèque de contenus en haute qualité. Une expérience interactive pensée pour les amateurs de vidéos et de divertissement.',
       category:'Mobile App'
     }
   ];
